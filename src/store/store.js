3d3e393a1a4092654ef1cef83a6f02adcfc3732f
seonit <seonit08@gmail.com>
2025-09-17 17:43:06 +0900

    250917 FESTIVAL_LIST01 & FESTIVAL_DETAIL 구현 완료

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import festivalReducer from './slices/festivalSlice.js';
+import FestivalShowReducer from './slices/festivalShowSlice.js';
 
 export default configureStore({
   reducer: {
     festival: festivalReducer,
+    festivalShow: FestivalShowReducer,
   }
-})
+});
